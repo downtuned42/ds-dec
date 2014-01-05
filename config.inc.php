0ec30c6255b074a9588499d63131a27fbbd16e0d
Domenik Niemietz <domenik@me.com>
2014-01-05 19:54:03 +0100

    changed info in config.inc.php to disable debug (NOTICE) messages :)

--- config.inc.php
+++ config.inc.php
@@ -7,7 +7,7 @@ ini_set('display_errors', 1);
 
 //****
 //comment this out to disable debug (NOTICE) messages
-//error_reporting(0);
+//ini_set('display_errors', 0);
 
 // endpoint of the diskstation web-api
 // if these scripts are hosted on the diskstation as well 'localhost' will do fine
